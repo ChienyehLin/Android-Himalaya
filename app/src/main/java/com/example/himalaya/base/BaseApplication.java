0833b31f53754c337ec7d31a37100b427925e0e1
linchienyeh_jaden@outlook.com <linchienyeh_jaden@outlook.com>
2020-09-18 10:58:45 +0800

    Custom Notification Pending

--- app/src/main/java/com/example/himalaya/base/BaseApplication.java
+++ app/src/main/java/com/example/himalaya/base/BaseApplication.java
@@ -2,7 +2,9 @@ package com.example.himalaya.base;
 
 import android.app.Application;
 import android.os.Handler;
+import android.os.RemoteException;
 
+import com.example.himalaya.aidl.IMyAidlInterface;
 import com.example.himalaya.utils.LogUtil;
 import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
 import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
@@ -30,7 +32,6 @@ public class BaseApplication extends Application {
         //初始化LogUtil,可以确保是应用打出来的log,改为True就可以隐藏所有log
         LogUtil.init(this.getPackageName(),false);
 
-        sHandler= new Handler();
     }
 
     public  static  Handler getsHandler(){
